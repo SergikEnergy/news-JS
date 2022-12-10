168c00efcdde4e9849f0fe7b2af8418b63e84bef
SergikEnergy <kis8883244@gmail.com>
2022-12-10 23:10:40 +0300

    feat: add config for webpack and EsLint

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '5b0d1731bf114953b47f015bb155a954', // получите свой ключ https://newsapi.org/
         });
     }
 }
