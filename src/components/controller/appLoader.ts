cf1eed6dcf45792aa9536ea38fad1a2f184c2233
SergikEnergy <kis8883244@gmail.com>
2022-12-14 02:08:12 +0300

    fix: change img placeholder

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.up.railway.app/', {
             apiKey: 'd54cfce6b08f49178f117662527108f1', // получите свой ключ https://newsapi.org/
         });
     }
--- src/components/view/news/news.ts
+++ src/components/view/news/news.ts
@@ -14,9 +14,9 @@ class News {
 
                 if (idx % 2) (newsClone.querySelector('.news__item') as HTMLDivElement).classList.add('alt');
 
-                (newsClone.querySelector('.news__meta-photo') as HTMLDivElement).style.backgroundImage = `url(${
-                    item.urlToImage || 'img/news_placeholder.jpg'
-                })`;
+                (newsClone.querySelector(
+                    '.news__meta-photo'
+                ) as HTMLDivElement).style.backgroundImage = `url(${item.urlToImage})`;
                 (newsClone.querySelector('.news__meta-author') as HTMLDivElement).textContent =
                     item.author || item.source.name;
                 (newsClone.querySelector('.news__meta-date') as HTMLDivElement).textContent = item.publishedAt
